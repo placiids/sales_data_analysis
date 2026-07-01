68a7d62f7602038d5d7c4fe5cbc4fc7adea35d7f
placiids <sabashaikh37879@gmail.com>
2026-07-01 20:37:31 +0530

    Create README.md

---
+++ README.m
@@ -0,0 +1,22 @@
+# Sales Data Analysis
+
+## Objective
+Analyze sales data using Python and visualize sales trends.
+
+## Tools Used
+- Python
+- Pandas
+- Matplotlib
+- Seaborn
+- Jupyter Notebook
+
+## Dataset
+sales_data_sample.csv
+
+## Tasks Performed
+- Loaded the dataset
+- Cleaned the data
+- Calculated total, average, maximum and minimum sales
+- Analyzed product line and territory sales
+- Created charts
+- Observed sales trends
